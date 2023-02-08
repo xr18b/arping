a290feee7ada6d1c51c3886ded55980f86ccc773
XR18B <89408703+xr18b@users.noreply.github.com>
2023-02-08 09:41:30 +0100

    Moved the reverse-lookup capabilities to an optional arguments

--- arping.py
+++ arping.py
@@ -6,6 +6,11 @@ from lib import platform, arp
 
 
 def main(argv) -> None:
+    do_lookup = False
+
+    if '-n' in argv or '--name' in argv:
+        do_lookup = True
+
     if not platform.is_root():
         print('Need to be root')
         sys.exit(1)
@@ -14,7 +19,7 @@ def main(argv) -> None:
 
     for _iface in ifaces:
         print('Scanning interface {0}, with IP {1}:'.format(_iface.name, _iface.subnet))
-        hosts = arp.pingsweep(_iface.subnet)
+        hosts = arp.pingsweep(net = _iface.subnet, do_lookup = do_lookup)
         for _host in hosts:
             print('\t{0} - {1}{2}'.format(_host[0], _host[1], _host[2]))
 
